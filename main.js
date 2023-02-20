00efadeaa2d13387e908d756d038d69082d3516c
Volodymyr Isyk <volodka.isyk.95@gmail.com>
2023-02-20 13:16:39 +0200

    feat: created header

---
+++ main.js
@@ -0,0 +1,25 @@
+let openBtn = document.querySelector('#open-btn');
+let btnImg = document.querySelector('.open_btn__img')
+let headerRow = document.querySelector('#header-row')
+
+openBtn.addEventListener("click", () => {
+    
+openBtn.addEventListener('click', () => {
+    if (headerRow.closest('.open')) {
+        btnImg.src = "src/header/open_btn.svg";
+        headerRow.classList.remove('open');
+    }
+    else {
+        headerRow.classList.add('open');
+        btnImg.src = "src/header/close_btn.svg";
+    }
+})
+
+headerRow.addEventListener("click", (e) => {
+    if (e.target.tagName == "A") {
+        document.querySelector('.header-row').classList.remove('open');
+        btnImg.src = "src/header/open_btn.svg";
+    }
+})
+
+
